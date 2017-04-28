cb0af5b53f38f2c4edf709dfe16dbd95364c0c64
staticfox <staticfox@staticfox.net>
2017-04-28 18:29:52 -0400

    Make the navbar a component instead

---
+++ app/components/nav-bar.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  session: Ember.inject.service('session'),
+
+  actions: {
+    invalidateSession() {
+      let notify = this.get('notify');
+
+      this.get('session').invalidate().then(() => {
+        notify.success('You have been logged out', {
+          closeAfter: 10000
+        });
+      }).catch((reason) => {
+        let message = 'Error logging you out: ';
+        if (reason.responseJSON.message instanceof Object) {
+          message += 'API error: Please try again.';
+        } else {
+          message += reason.responseJSON.message;
+        }
+        notify.alert(message, {
+          closeAfter: 10000
+        });
+      });
+    }
+  }
+});
