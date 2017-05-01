c07afb917d81ee9791401f04831f5d5570184e96
staticfox <staticfox@staticfox.net>
2017-04-30 22:50:04 -0400

    cli-build: Remove js-cookie requirement

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -23,7 +23,6 @@ module.exports = function(defaults) {
   app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
   app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
   app.import('bower_components/jquery-validation/dist/jquery.validate.min.js');
-  app.import('bower_components/js-cookie/src/js.cookie.js');
 
   return app.toTree();
 };
