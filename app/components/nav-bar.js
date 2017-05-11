7e0693c48d1aa2a1a82f0e1bab6d765cefb9ce83
staticfox <staticfox@staticfox.net>
2017-05-10 20:47:25 -0400

    nav-bar: Make sure we inject the notify service

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   session: Ember.inject.service('session'),
+  notify: Ember.inject.service('notify'),
 
   actions: {
     invalidateSession() {
