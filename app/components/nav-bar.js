74e8a13a71355668a5e89c9db1b3c6282ef3abd0
staticfox <staticfox@staticfox.net>
2017-05-10 20:47:49 -0400

    nav-bar: Don't assume we got a response back at all

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -14,11 +14,17 @@ export default Ember.Component.extend({
         });
       }).catch((reason) => {
         let message = 'Error logging you out: ';
-        if (reason.responseJSON.message instanceof Object) {
+
+        if (reason.responseJSON == undefined) {
+          // API Offline
+          message += 'API error: Please try again.';
+        } else if (reason.responseJSON.message instanceof Object) {
+          // API Error
           message += 'API error: Please try again.';
         } else {
           message += reason.responseJSON.message;
         }
+
         notify.alert(message, {
           closeAfter: 10000
         });
